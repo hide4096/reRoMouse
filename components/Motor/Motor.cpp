c47f7e356ab45101981a38b40cfee614fec48d17
Yu Shi <s.yujiro613@gmail.com>
2025-02-21 06:51:08 +0900

    [Update] モータPWM周波数変更

--- components/Motor/Motor.cpp
+++ components/Motor/Motor.cpp
@@ -1,7 +1,7 @@
 #include "Motor.hpp"
 
 #define BDC_MCPWM_TIMER_RESOLUTION_HZ 10000000                                      // 10MHz, 1 tick = 0.1us
-#define BDC_MCPWM_FREQ_HZ 250000                                                    // 250KHz PWM
+#define BDC_MCPWM_FREQ_HZ 150000                                                    // 250KHz PWM
 #define BDC_MCPWM_DUTY_TICK_MAX (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
 
 mcpwm_cmpr_handle_t comparator_r = NULL;
