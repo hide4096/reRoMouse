edecc825ba12c49299eb13fdce7eca83b7033dba
Urojiuyu <s.yujiro613@gmail.com>
2025-02-22 06:28:04 +0900

    [Update] コメント文修正

--- components/Motor/Motor.cpp
+++ components/Motor/Motor.cpp
@@ -2,7 +2,7 @@
 
 #define BDC_MCPWM_TIMER_RESOLUTION_HZ 10000000                                      // 10MHz, 1 tick = 0.1us
 #define BDC_MCPWM_FREQ_HZ 150000                                                    // 250KHz PWM
-#define BDC_MCPWM_DUTY_TICK_MAX (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
+#define BDC_MCPWM_DUTY_TICK_MAX (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks (=PWM分解能 40(5bit))
 
 mcpwm_cmpr_handle_t comparator_r = NULL;
 mcpwm_cmpr_handle_t comparator_l = NULL;
