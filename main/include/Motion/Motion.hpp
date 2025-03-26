fc7858b760869863b19350d2b572873e149820b5
Urojiuyu <s.yujiro613@gmail.com>
2025-03-26 19:46:57 +0900

    [Add] 最短走行用関数追加

--- main/include/Motion/Motion.hpp
+++ main/include/Motion/Motion.hpp
@@ -41,7 +41,9 @@ class Motion : public Micromouse
         void offset2();
         void calibrate_wall_th();
         void fast_straight(uint8_t straight_count);
+        void fast_stop(uint8_t straight_count);
         void CheckMotorDuty(float duty_l, float duty_r, uint32_t time);
+        float CalcVelocity(float dis, float vel, float acc);
         
         
     protected:
