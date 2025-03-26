9584cf5788163bbea55c12fe72c966070e92ae94
Urojiuyu <s.yujiro613@gmail.com>
2025-03-26 19:43:33 +0900

    [Update] マップサイズ更新

--- main/files.cpp
+++ main/files.cpp
@@ -1,7 +1,7 @@
 #include "files.hpp"
 
-#define MAZESIZE_X 32
-#define MAZESIZE_Y 32
+#define MAZESIZE_X 16
+#define MAZESIZE_Y 16
 
 const char *PID_FILE_TAG = "file_pid";
 const char *WALL_TH_FILE_TAG = "file_wall_th";
